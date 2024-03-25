fc42cf3dab17e7f55cc67208d3911c8cce945926
Ashleigh Carter <ashbcarter@gmail.com>
2024-03-25 11:47:05 -0400

    added post route in books.router

--- server/routes/books.router.js
+++ server/routes/books.router.js
@@ -26,7 +26,20 @@ router.get('/', (req, res) => {
  * POST route template
  */
 router.post('/', (req, res) => {
-  // POST route code here
+  const queryText = `INSERT INTO "books" ("title", "author", "pubdate", "pagecount", "cover")
+  VALUES ($1, $2, $3, $4, $5);`;
+
+  const { title, author, pubdate, pagecount, cover } = req.body;
+
+  pool
+    .query(queryText, [title, author, pubdate, pagecount, cover])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log('server post books error', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
