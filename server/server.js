2fbfc6d2f1f98cebcd8d739d1f1324a6bb834453
Ashleigh Carter <ashbcarter@gmail.com>
2024-03-27 18:42:10 -0400

    added goals router info into server.js. created goals.router component

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route Includes
 const userRouter = require('./routes/user.router');
 const booksRouter = require('./routes/books.router');
+const goalsRouter = require('./routes/goals.router');
 
 // Express Middleware
 app.use(express.json());
@@ -26,6 +27,7 @@ app.use(passport.session());
 // Routes
 app.use('/api/user', userRouter);
 app.use('/api/books', booksRouter);
+app.use('/api/goals', goalsRouter);
 
 // Listen Server & Port
 app.listen(PORT, () => {
--- src/components/AddGoalForm/AddGoalForm.jsx
+++ src/components/AddGoalForm/AddGoalForm.jsx
@@ -1,11 +1,5 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import InputLabel from '@mui/material/InputLabel';
-// import MenuItem from '@mui/material/MenuItem';
-// // import FormControl from '@mui/material/FormControl';
-// import Select from '@mui/material/Select';
 
 import DeadlineButton from '../DeadlineButton/DeadlineButton';
 import ChpPgButton from '../ChpPgButton/ChpPgButton';
@@ -13,15 +7,6 @@ import ChpPgButton from '../ChpPgButton/ChpPgButton';
 function GoalForm() {
   const [title, setTitle] = useState('');
   const [number, setNumber] = useState('');
-  // const [chp_pgs, setChp_Pgs] = React.useState('');
-  // const [chapter, setChapter] = React.useState('');
-  // const [pages, setPages] = React.useState('');
-
-  // const handleChange = (event) => {
-  //   setChp_Pgs(event.target.value);
-  //   // setChapter(event.target.value);
-  //   // setPages(event.target.value);
-  // };
 
   return (
     <section>
@@ -38,27 +23,12 @@ function GoalForm() {
           value={number}
           onChange={(event) => setNumber(event.target.value)}
         />
-        {/* <Box sx={{ minWidth: 120 }}>
-          {/* <FormControl fullWidth> */}
-        {/* <InputLabel id="demo-simple-select-label">Chp/Pgs</InputLabel> */}
-        {/* <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={chp_pgs}
-            label="Chp/Pgs"
-            onChange={handleChange}
-          > */}
-        {/* <MenuItem value={1}>Chapter(s)</MenuItem> */}
-        {/* <MenuItem value={2}>Page(s)</MenuItem> */}
-        {/* </Select> */}
-        {/* </FormControl> */}
-        {/* </Box>  */}
 
         <ChpPgButton />
         <DeadlineButton />
 
         <Link to="user">
-          <button type="submit">Save New Goal</button>
+          <button type="submit">Save New Goal!</button>
         </Link>
       </form>
     </section>
